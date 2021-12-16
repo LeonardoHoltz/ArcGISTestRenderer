3a619d2bfdcc40e08b3607eec65138fe6d0b318c
Leonardo Holtz <leo.holtz98@gmail.com>
2021-12-16 16:31:16 -0300

    Adicionar arquivos de projeto.

---
+++ ArcGISTestRenderer/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using System.Drawing;
+
+namespace ArcGISTestRenderer
+{
+    public sealed partial class MainPage : Page
+    {
+        public MapViewModel MapViewModel { get; set; }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            MapViewModel = new MapViewModel();
+            Esri.ArcGISRuntime.ArcGISRuntimeEnvironment.ApiKey = "AAPK1c1fc5824d93475092062f6ca098d7e7yoLb20wEM1QQUqVkgCc8K8AE8HA1YRVj25IQZZdhGfkxj0wgk50Bn9TVR4agbplF";
+            MapPoint mapCenterPoint = new MapPoint(-118.805, 34.027, SpatialReferences.Wgs84);
+            MapViewTest.SetViewpointAsync(new Viewpoint(mapCenterPoint, 100000));
+            MapViewTest.GraphicsOverlays = MapViewModel.GraphicsOverlays;
+        }
+    }
+}
---
+++ ArcGISTestRenderer/MapViewModel.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Symbology;
+using Esri.ArcGISRuntime.UI;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Esri.ArcGISRuntime.Geometry;
+
+namespace ArcGISTestRenderer
+{
+    public class MapViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private Map _map;
+        public Map Map
+        {
+            get
+            {
+                return _map;
+            }
+            set
+            {
+                _map = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private GraphicsOverlayCollection _graphicsOverlays;
+        public GraphicsOverlayCollection GraphicsOverlays
+        {
+            get
+            {
+                return _graphicsOverlays;
+            }
+            set
+            {
+                _graphicsOverlays = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MapViewModel()
+        {
+            SetupMap();
+            CreateGraphics();
+        }
+
+        private void SetupMap()
+        {
+            // Create a new map with a 'topographic vector' basemap.
+            Map = new Map(BasemapStyle.ArcGISTopographic);
+        }
+
+        private void CreateGraphics()
+        {
+            // Create a new graphics overlay to contain a variety of graphics.
+            GraphicsOverlay malibuGraphicsOverlay = new GraphicsOverlay();
+
+            // Add the overlay to a graphics overlay collection.
+            GraphicsOverlayCollection overlays = new GraphicsOverlayCollection
+            {
+                malibuGraphicsOverlay
+            };
+
+            // Set the view model's "GraphicsOverlays" property (will be consumed by the map view).
+            GraphicsOverlays = overlays;
+
+            
+            // Add the point graphic to graphics overlay.
+            malibuGraphicsOverlay.Graphics.Add(CreatePoint());
+        }
+
+        private Graphic CreatePoint()
+        {
+            // Create a point geometry.
+            var dumeBeachPoint = new MapPoint(-118.8066, 34.0006, SpatialReferences.Wgs84);
+
+            // Create a symbol to define how the point is displayed.
+            var pointSymbol = new SimpleMarkerSymbol
+            {
+                Style = SimpleMarkerSymbolStyle.Circle,
+                Color = System.Drawing.Color.Orange,
+                Size = 10.0
+            };
+
+            // Add an outline to the symbol.
+            pointSymbol.Outline = new SimpleLineSymbol
+            {
+                Style = SimpleLineSymbolStyle.Solid,
+                Color = System.Drawing.Color.Blue,
+                Width = 2.0
+            };
+
+            // Create a point graphic with the geometry and symbol.
+            return new Graphic(dumeBeachPoint, pointSymbol);
+
+        }
+    }
+}
